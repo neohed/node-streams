9fce7b05a98c444b98b8724dd61b73fbdb1401af
neohed <35173474+neohed@users.noreply.github.com>
2021-07-20 13:59:29 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const fs = require("fs");
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const file_path = path.join(__dirname, '../../data/4300-0.txt');
+  const rs = fs.createReadStream(file_path);
+  const { size } = fs.statSync(file_path);
+
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Length", size);
+
+  rs.pipe(res);
 });
 
 module.exports = router;
